6ca511a632c249ea242f7831409bdda9ee24524f
Alexander <alexander.ivanofff@gmail.com>
2021-05-07 15:47:52 +0300

    Deleted unnecessary text from server

--- server/create_py_server.py
+++ server/create_py_server.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import json
@@ -41,8 +36,6 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
-        # logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-        #              str(self.path), str(self.headers), post_data.decode('utf-8'))
         print(post_data.decode('utf-8'))
 
         self._set_response('application/json')
